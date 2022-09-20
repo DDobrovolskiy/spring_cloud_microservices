f4155fc70896c72dc8f9fb71fca590d2d831e73f
Dmitriy Dobrovolskiy <dda19081990@gmail.com>
2022-09-20 14:51:05 +0500

    add redis

--- cloud_order/src/main/java/ru/cloud/order/entity/cashed/Token.java
+++ cloud_order/src/main/java/ru/cloud/order/entity/cashed/Token.java
@@ -3,7 +3,9 @@ package ru.cloud.order.entity.cashed;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import java.io.Serializable;
 
@@ -16,7 +18,9 @@ import java.io.Serializable;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Token implements Serializable {
+public class Token {
+    @Id
+    @Indexed // Нужно для возможности поиска по полю
     private String id;
     private String token;
 }
